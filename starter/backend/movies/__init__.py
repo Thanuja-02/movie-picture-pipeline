0427a149b1b85a02c09174c981ceda6f7e200806
Thanuja-02 <bommireddythanuja@gmail.com>
2026-08-12 14:59:53 +0530

    Fix Changes

--- starter/backend/movies/__init__.py
+++ starter/backend/movies/__init__.py
@@ -1,9 +1,12 @@
-from flask import Blueprint
-from .resources import Movies
+import os
+from flask import Flask
+from flask_cors import CORS
+from backend.movies.movies_api import movies_api
 
-movies_api = Blueprint("movies_api", __name__)
-movies = Movies.as_view("movies")
 
-movies_api.add_url_rule("/movies", strict_slashes=False, defaults={"movie_id": None}, view_func=movies, methods=["GET"])
-movies_api.add_url_rule("/movies", view_func=movies, methods=["POST"])
-movies_api.add_url_rule("/movies/<int:movie_id>", view_func=movies, methods=["GET", "PUT", "DELETE"])
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(movies_api)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
